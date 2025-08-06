a4d5fd35d08d4c9150f3f1f466aa00b243ae25ef
gharedaghi.f <gharedaghi.f@DESKTOP-3QG5LJJ>
2025-08-06 13:26:35 +0330

    first commit

---
+++ First.ConsoleApp/First.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Amirali!");
+ 
+Console.WriteLine("Enter number:");
+var num1 = Console.ReadLine();
+
+
+Console.WriteLine("Enter number:");
+var num2 = Console.ReadLine();
+
+var num3 = Int32.Parse(num1) + Int32.Parse(num2);
+Console.WriteLine($"{num1} + {num2} = {num3}");
